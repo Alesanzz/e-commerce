1a1656d49a9b005e08f895b8a7f0d96acad37110
Alesanzz <alejandro240295@hotmail.com>
2023-05-25 18:19:47 -0300

    Addi function & visualization of the server socket

--- source/controller/products-realtime-controller.js
+++ source/controller/products-realtime-controller.js
@@ -1,11 +1,14 @@
 //importando las funciones de la clase product manager
 import { productManager } from "../modules/products-manager.js";
-const products = productManager.getProducts();
 
 export const productRealtimeController = {
-    index: function(req, res) {
-        console.log("cliente conectado")
-        let products = productManager.getProducts();
-        return res.render("real-time-products", { products });
-    },
-}
+  index: async function (req, res) {
+    try {
+      console.log("cliente conectado");
+      const products = await productManager.getProducts();
+      return res.render("real-time-products", { title: "Socket Server", products: products });
+    } catch (error) {
+      return res.status(500).json({ succes: "false", msg: error });
+    }
+  },
+};
